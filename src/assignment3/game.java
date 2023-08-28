f46a2418babbacee16edd5e14785a985fb5608e2
Jackson Baumgart <jacksonbaumgart11@gmail.com>
2023-08-28 01:50:27 -0500

    Assignment 3 in progress. Final check needed before submission.

--- src/assignment3/game.java
+++ src/assignment3/game.java
@@ -1,7 +1,86 @@
+// Assignment 3b TODO
 package assignment3;
 
 public class game {
     public static void main(String[] args) {
 
+        // Create "world".
+        char[][] world = new char[10][10];
+
+        // Fills world with periods.
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j <= 9; j++) {
+                world[i][j] = '.';
+            }
+        }
+
+        // Declare instances of enemies.
+        Enemy orc = new Enemy ("Orc");
+        Enemy troll = new Enemy("Troll");
+        Enemy ghoul = new Enemy("Ghoul");
+        Enemy demon = new Enemy("Demon");
+
+        // Display enemies on world.
+        world[orc.Xpos][orc.Ypos] = orc.Avatar;
+        world[troll.Xpos][troll.Ypos] = troll.Avatar;
+        world[ghoul.Xpos][ghoul.Ypos] = ghoul.Avatar;
+        world[demon.Xpos][demon.Ypos] = demon.Avatar;
+
+        // Prints world.
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j <= 9; j++) {
+                System.out.print(world[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    }
+
+class Enemy {
+    // Variables
+    public int Xpos;
+    public int Ypos;
+    public char Avatar;
+    public String Type;
+    public int HP;
+    public int Attack;
+    public int Armor;
+
+    // Constructor
+    Enemy(String theType) {
+        // Type from declaration, position random.
+        Type = theType;
+        Xpos = (int) (Math.random() * 9) + 1;
+        Ypos = (int) (Math.random() * 9) + 1;
+
+        // Rest dependent on type.
+        if (Type.equals("Orc")) {
+            Avatar = 'O';
+            HP = 20;
+            Attack = 5;
+            Armor = 20;
+        }
+
+        if (Type.equals("Troll")) {
+            Avatar = 'T';
+            HP = 10;
+            Attack = 2;
+            Armor = 5;
+        }
+
+        if (Type.equals("Ghoul")) {
+            Avatar = 'G';
+            HP = 50;
+            Attack = 15;
+            Armor = 100;
+        }
+
+        if (Type.equals("Demon")) {
+            Avatar = 'D';
+            HP = 100;
+            Attack = 20;
+            Armor = 100;
+        }
     }
 }
--- src/assignment3/shapes.java
+++ src/assignment3/shapes.java
@@ -1,8 +1,87 @@
+// Assignment 3a DONE
 package assignment3;
+import java.util.*;
 
 public class shapes {
     public static void main(String[] args) {
+        // Declare scanner.
+        Scanner in = new Scanner(System.in);
 
+        // Ask user for pyramid height.
+        System.out.print("Input pyramid width (meters) >> ");
+        double pyramidWidth = in.nextDouble();
+        // Ask user for pyramid length.
+        System.out.print("Input pyramid height (meters) >> ");
+        double pyramidHeight = in.nextDouble();
+        System.out.println();
+
+        // Declare instance of pyramid.
+        Pyramid pyramid1 = new Pyramid(pyramidWidth, pyramidHeight);
+
+        // Print pyramid volume.
+        System.out.println("Pyramid volume is " + pyramid1.getVolume() + " cubic meters.");
+        // Print pyramid surface area.
+        System.out.println("Pyramid surface area is " + pyramid1.getSurfaceArea() + " square meters.");
+        System.out.println();
+
+        // Ask user for cone radius.
+        System.out.print("Input cone radius (meters) >> ");
+        double coneRadius = in.nextDouble();
+        // Ask user for cone height.
+        System.out.print("Input cone height (meters) >> ");
+        double coneHeight = in.nextDouble();
+        System.out.println();
+
+        // Declare instance of cone.
+        Cone cone1 = new Cone(coneRadius, coneHeight);
+
+        // Print cone volume.
+        System.out.println("Cone volume is " + cone1.getVolume() + " cubic meters.");
+        // Print cone surface area.
+        System.out.println("Cone surface area is " + cone1.getSurfaceArea() + " square meters.");
     }
-    
+}
+
+class Pyramid {
+    // Variables
+    public double Width;
+    public double Height;
+
+    // Constructor
+    public Pyramid (double W, double H){
+        Width = W;
+        Height = H;
+    }
+
+    // Volume method
+    double getVolume() {
+        return Math.pow(Width, 2) * (Height / 3);
+    }
+
+    // Surface area method
+    double getSurfaceArea() {
+        return Math.pow(Width, 2) + 2 * Width * Math.sqrt(Math.pow(Width, 2) / 4 + Math.pow(Height, 2));
+    }
+}
+
+class Cone {
+    // Variables
+    public double Radius;
+    public double Height;
+
+    // Constructor
+    public Cone(double R, double H) {
+        Radius = R;
+        Height = H;
+    }
+
+    // Volume method
+    double getVolume() {
+        return Math.PI * Math.pow(Radius, 2) * (Height / 3);
+    }
+
+    // Surface area method
+    double getSurfaceArea() {
+        return Math.PI * Radius * (Radius + Math.sqrt(Math.pow(Height, 2) + Math.pow(Radius, 2)));
+    }
 }
