55e8130ecb931fa5d3724089d8b66d373b358d9e
Jackson Baumgart <jacksonbaumgart11@gmail.com>
2023-08-29 16:03:54 -0500

    Assignment 5 in progress. Extra credit implementation needed. Table alignment fix needed. Marked with TODO.

--- src/assignment5/bank.java
+++ src/assignment5/bank.java
@@ -33,8 +33,8 @@ public class bank {
             System.out.println("| 3 Deposit Money");
             System.out.println("| 4 Withdraw Money");
             System.out.println("| 5 Print All Loan Accounts");
-            System.out.println("| 6 Make Loan");
-            System.out.println("| 7 Make Payment");
+            System.out.println("| 6 Make New Loan");
+            System.out.println("| 7 Make Loan Payment");
             System.out.println("| 8 Add Customer");
             System.out.println("| 9 Delete Customer");
             System.out.println("| 0 Exit System");
@@ -45,27 +45,93 @@ public class bank {
 
             if (selection == 1) {
                 System.out.println("====================================================");
-                System.out.println("| Bank Information");
+                System.out.println("| Show Bank Information");
                 System.out.println("| ");
                 System.out.println("| Total Bank Balance " + Check.get(0).BankBalance);
                 System.out.println("| Total Bank Customers " + Check.get(0).NumberCustomers);
                 System.out.println("====================================================");
             } else if (selection == 2) {
-                // TODO: Option 2
+                // TODO: Fix table alignment.
+                System.out.println("====================================================");
+                System.out.println("| Show All Checking Accounts");
+                System.out.println("| ");
+                System.out.println("| Rec\tName\tBalance\tTransactions");
+                System.out.println("------------------------------------------------");
+                for (int i=0; i<Check.size(); i++)
+                {
+                    System.out.println ("| " + i + "\t" + Check.get(i).GetLName() + ", " + Check.get(i).GetFName() + "\t" + Check.get(i).getCheckingBalance() + "\t" + Check.get(i).CustomerTransactions);
+                }
+                System.out.println("====================================================");
             } else if (selection == 3) {
-                // TODO: Option 3
+                System.out.println("====================================================");
+                System.out.println("| Make a Deposit");
+                System.out.println("| ");
+                System.out.print("| Enter Record Number >> ");
+                int recNum = in.nextInt();
+                System.out.print("| Enter Deposit Amount >> ");
+                double depositAmount = in.nextDouble();
+                Check.get(recNum).Deposit(depositAmount);
+                System.out.println("| ");
+                System.out.println("| Deposit Successful");
+                System.out.println("| " + Check.get(recNum).GetLName() + ", " + Check.get(recNum).GetFName() + " now has $" + Check.get(recNum).getCheckingBalance());
+                System.out.println("====================================================");
             } else if (selection == 4) {
-                // TODO: Option 4
+                System.out.println("====================================================");
+                System.out.println("| Make a Withdraw");
+                System.out.println("| ");
+                System.out.print("| Enter Record Number >> ");
+                int recNum = in.nextInt();
+                System.out.print("| Enter Withdraw Amount >> ");
+                double withdrawAmount = in.nextDouble();
+                System.out.println("| ");
+                System.out.println("| Withdraw Successful");
+                Check.get(recNum).Withdraw(withdrawAmount);
+                System.out.println("| Amount Withdrawn is $" + withdrawAmount);
+                System.out.println("| " + Check.get(recNum).GetLName() + ", " + Check.get(recNum).GetFName() + " now has $" + Check.get(recNum).getCheckingBalance());
+                System.out.println("====================================================");
             } else if (selection == 5) {
-                // TODO: Option 5
+                // TODO: Fix table alignment.
+                System.out.println("====================================================");
+                System.out.println("| Show All Loan Accounts");
+                System.out.println("| ");
+                System.out.println("| Rec\tName\tBalance\tTransactions");
+                System.out.println("------------------------------------------------");
+                for (int i=0; i<Loan.size(); i++)
+                {
+                    System.out.println ("| " + i + "\t" + Loan.get(i).GetLName() + ", " + Loan.get(i).GetFName() + "\t" + Loan.get(i).getLoanBalance() + "\t" + Loan.get(i).CustomerTransactions);
+                }
+                System.out.println("====================================================");
             } else if (selection == 6) {
-                // TODO: Option 6
+                System.out.println("====================================================");
+                System.out.println("| Make New Loan");
+                System.out.println("| ");
+                System.out.print("| Enter Record Number >> ");
+                int recNum = in.nextInt();
+                System.out.print("| Enter Loan Amount >> ");
+                double loanAmount = in.nextDouble();
+                Loan.get(recNum).MakeLoan(loanAmount);
+                System.out.println("| ");
+                System.out.println("| Loan Creation Successful");
+                System.out.println("| " + Loan.get(recNum).GetLName() + ", " + Loan.get(recNum).GetFName() + " loan balance is now $" + Loan.get(recNum).getLoanBalance());
+                System.out.println("====================================================");
             } else if (selection == 7) {
-                // TODO: Option 7
+                System.out.println("====================================================");
+                System.out.println("| Make Loan Payment");
+                System.out.println("| ");
+                System.out.print("| Enter Record Number >> ");
+                int recNum = in.nextInt();
+                System.out.print("| Enter Payment Amount >> ");
+                double paymentAmount = in.nextDouble();
+                System.out.println("| ");
+                System.out.println("| Payment Successful");
+                Loan.get(recNum).MakePayment(paymentAmount);
+                System.out.println("| Payment Amount is $" + paymentAmount);
+                System.out.println("| " + Loan.get(recNum).GetLName() + ", " + Loan.get(recNum).GetFName() + " loan balance is now $" + Loan.get(recNum).getLoanBalance());
+                System.out.println("====================================================");
             } else if (selection == 8) {
-                // TODO: Option 8
+                // TODO: Option 8 EC
             } else if (selection == 9) {
-                // TODO: Option 9
+                // TODO: Option 9 EC
             } else if (selection == 0) {
                 System.out.println("====================================================");
                 System.out.println("| System Exit Successful");
@@ -120,6 +186,14 @@ class CheckingAccount extends Customer {
         return CheckingBalance;
     }
 
+    public String GetFName() {
+        return FName;
+    }
+
+    public String GetLName() {
+        return LName;
+    }
+
     void Deposit(double Amount) {
         CheckingBalance += Amount;
         CustomerTransactions++;
@@ -129,6 +203,7 @@ class CheckingAccount extends Customer {
     void Withdraw(double Amount) {
         if (CheckingBalance - Amount < 0.00) {
             CheckingBalance -= 25;
+            System.out.println("| Overdraft fee of $25.0 Assessed");
         }
         CheckingBalance -= Amount;
         CustomerTransactions++;
@@ -158,6 +233,14 @@ class LoanAccount extends Customer {
         return LoanBalance;
     }
 
+    public String GetFName() {
+        return FName;
+    }
+
+    public String GetLName() {
+        return LName;
+    }
+
     void MakeLoan(double Amount) {
         LoanBalance += Amount * 1.25;
         CustomerTransactions++;
@@ -167,7 +250,7 @@ class LoanAccount extends Customer {
     void MakePayment(double Amount) {
         LoanBalance -= Amount;
         if (LoanBalance <= 0) {
-            System.out.println("Your loan has been paid off.");
+            System.out.println("| Loan fully paid off. No further payments required.");
         }
         CustomerTransactions++;
         BankBalance += Amount;
